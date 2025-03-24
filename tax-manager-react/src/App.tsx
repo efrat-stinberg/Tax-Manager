3648444d635d5502ad8040c87d20a98e6b572636
efrat-stinberg <efrat90906@gmail.com>
2025-03-24 14:40:05 +0200

    commit all

--- tax-manager-react/src/App.tsx
+++ tax-manager-react/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Provider, useSelector } from "react-redux";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import store from "./store/store";
 import Login from "./components/Login";
 import UserAvatar from "./components/UserAvatar";
 import FolderUpload from "./components/FolderUploud";
+
 
 const headerStyle = {
   display: 'flex',
@@ -31,16 +32,15 @@ function Main() {
   return (
     <div style={headerStyle}>
       {isAuthenticated ? (
-        <Routes>
-          <Route path="/upload/:folderName" element={<FolderUpload />} />
-          <Route path="/" element={<UserAvatar />} />
-        </Routes>
+        <>
+          <FolderUpload /> {/* Render FolderUpload directly */}
+          <UserAvatar /> {/* Render UserAvatar directly */}
+        </>
       ) : (
         <Login />
       )}
     </div>
   );
 }
-
 
 export default App;
